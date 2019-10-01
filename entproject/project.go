7a480b4b1e5839686b8da42969e44d5887589438
Shannon Roberts <sneakypuppers@gmail.com>
2019-10-01 13:32:41 +1300

    update

--- entproject/project.go
+++ entproject/project.go
@@ -1,6 +1,69 @@
 package project
 
-//hi
+import (
+	"log"
+	"net/http"
+
+	"time"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type Note struct {
+	NoteID      int       `json: noteID`
+	Title       string    `json: title`
+	Contents    string    `json: contents`
+	DateCreated time.Time `json: dateCreated`
+	DateUpdated time.Time `json dateUpdated`
+}
+
+type User struct {
+	UserID     int    `json: userID`
+	GivenName  string `json: givenName`
+	FamilyName string `json: familyName`
+}
+
+type NoteAcess struct {
+	NoteAccessID int  `json: noteAccessID`
+	NoteID       int  `json: noteID`
+	UserID       int  `json: userID`
+	Read         bool `json: read`
+	Write        bool `json: write`
+}
+
 func main() {
+	//Router
+	r := mux.NewRouter()
+	//Route Handlers
+	r.HandleFunc("/Notes", getNotes).Methods("GET")
+	r.HandleFunc("/Notes/{NoteID}", getNote).Methods("GET")
+	r.HandleFunc("/Notes", createNote).Methods("POST")
+	r.HandleFunc("/Notes/{NoteID}", updateNote).Methods("PUT")
+	r.HandleFunc("/Notes/{NoteID}", deleteNote).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func getNotes(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	//json.NewEncoder(w).Encode()
+}
+
+func getNote(w http.ResponseWriter, r *http.Request) {
 
 }
+
+func createNote(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateNote(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteNote(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//hi
